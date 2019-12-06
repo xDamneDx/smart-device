58e796900f7bef96ea5eca93cf18a0cf2a0df1e9
Владимир Ливинский <xDamneDx@Gmail.com>
2019-12-06 11:54:59 +0200

    Добавляет разметку, стилизацию и JS для всплывающего окна

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,67 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var KEYCODE = {
+    esc: 27
+  };
+  var link = document.querySelector('.page-header__contacts-button');
+  var popup = document.querySelector('.modal');
+  var close = popup.querySelector('.modal__close');
+  var form = popup.querySelector('.modal__form');
+  var userName = popup.querySelector('#call-name');
+  var phone = popup.querySelector('#call-phone');
+  var message = popup.querySelector('#call-question');
+  var isStorageSupport = true;
+  var storage = {};
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+
+  try {
+    storage.name = localStorage.getItem('name');
+    storage.phone = localStorage.getItem('phone');
+    storage.message = localStorage.getItem('message');
+  } catch (err) {
+    isStorageSupport = false;
   }
-});
+
+  link.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('modal--show');
+
+    if (storage.name) {
+      userName.value = storage.name;
+      phone.value = storage.phone;
+      message.value = storage.message;
+      message.focus();
+    } else {
+      userName.focus();
+    }
+  });
+
+  close.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.remove('modal--show');
+  });
+
+  form.addEventListener('submit', function () {
+    if (isStorageSupport) {
+      localStorage.setItem('name', userName.value);
+      localStorage.setItem('phone', phone.value);
+      localStorage.setItem('message', message.value);
+    }
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE.esc) {
+      evt.preventDefault();
+      if (popup.classList.contains('modal--show')) {
+        popup.classList.remove('modal--show');
+      }
+    }
+  });
+
+  popup.addEventListener('click', function (evt) {
+    if (evt.target === popup) {
+      popup.classList.remove('modal--show');
+    }
+  });
+})();
